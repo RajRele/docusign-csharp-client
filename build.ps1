6bf2216773b3dfa86415b2fb83cd8b16067168a8
bharat.rele <bharat.rele@docusign.com>
2018-07-30 21:56:30 -0700

    Color

--- build.ps1
+++ build.ps1
@@ -1,33 +1,33 @@
 try{
-Write-Host "Cleaning!" -ForegroundColor Orange
+Write-Host "Cleaning!" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /t:clean /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Restore DocuSign.eSign" -ForegroundColor Orange
+Write-Host "Restore DocuSign.eSign" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /t:restore /p:TargetFramework=net45 /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Building DocuSign.eSign for Net4.5" -ForegroundColor Orange
+Write-Host "Building DocuSign.eSign for Net4.5" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /p:TargetFramework=net45 /p:Configuration=Debug /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Restore DocuSign.eSign for NetStandard 2.0" -ForegroundColor Orange
+Write-Host "Restore DocuSign.eSign for NetStandard 2.0" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /t:restore /p:TargetFramework=netstandard2.0 /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Building DocuSign.eSign for NetStandard2.0" -ForegroundColor Orange
+Write-Host "Building DocuSign.eSign for NetStandard2.0" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /p:TargetFramework=netstandard2.0 /p:Configuration=Debug /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Cleaning SdkTests!" -ForegroundColor Orange
+Write-Host "Cleaning SdkTests!" -ForegroundColor Yellow
 msbuild ./test/SdkTests/SdkTests.csproj /t:clean /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
     
-Write-Host "Restoring and Building SdkTests for Net4.5" -ForegroundColor Orange
+Write-Host "Restoring and Building SdkTests for Net4.5" -ForegroundColor Yellow
 msbuild ./test/SdkTests/SdkTests.csproj /t:restore /p:TargetFramework=net45 /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "ReBuilding SdkTests for Net4.5" -ForegroundColor Orange
+Write-Host "ReBuilding SdkTests for Net4.5" -ForegroundColor Yellow
 msbuild ./test/SdkTests/SdkTests.csproj /t:rebuild /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 }
