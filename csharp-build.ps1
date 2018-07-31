cc2516cf652c021565688fc2bdd3a9d229baaced
bharat.rele <bharat.rele@docusign.com>
2018-07-31 12:01:33 -0700

    misc

--- csharp-build.ps1
+++ csharp-build.ps1
@@ -1,18 +1,18 @@
 try
 {
-Write-Host "Cleaning!" -ForegroundColor Yellow
+Write-Host "Cleaning DocuSign.eSign!" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /t:clean /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Restore DocuSign.eSign" -ForegroundColor Yellow
+Write-Host "`nRestore DocuSign.eSign" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /t:restore /p:TargetFramework=net45 /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Building DocuSign.eSign for Net4.5" -ForegroundColor Yellow
+Write-Host "`nBuilding DocuSign.eSign for Net45" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /p:TargetFramework=net45 /p:Configuration=Debug /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "Restore DocuSign.eSign for NetStandard 2.0" -ForegroundColor Yellow
+Write-Host "Restore DocuSign.eSign for NetStandard2.0" -ForegroundColor Yellow
 msbuild ./sdk/src/DocuSign.eSign/DocuSign.eSign.csproj /t:restore /p:TargetFramework=netstandard2.0 /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
@@ -23,12 +23,8 @@ if ($lastExitCode -ne 0) { exit $lastExitCode }
 Write-Host "Cleaning SdkTests!" -ForegroundColor Yellow
 msbuild ./test/SdkTests/SdkTests.csproj /t:clean /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
-    
-Write-Host "Restoring SdkTests for Net4.5" -ForegroundColor Yellow
-msbuild ./test/SdkTests/SdkTests.csproj /t:restore /p:TargetFramework=net45 /verbosity:minimal
-if ($lastExitCode -ne 0) { exit $lastExitCode }
 
-Write-Host "ReBuilding SdkTests for Net4.5" -ForegroundColor Yellow
+Write-Host "Re-Building SdkTests for Net45" -ForegroundColor Yellow
 nuget install ./test/SdkTests/packages.config -OutputDirectory ./test/packages
 msbuild ./test/SdkTests/SdkTests.csproj /t:rebuild /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
