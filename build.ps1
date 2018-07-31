1565d5e2e4fc7b322c07059b9b80a8c1f482bf67
bharat.rele <bharat.rele@docusign.com>
2018-07-30 23:14:46 -0700

    restore

--- build.ps1
+++ build.ps1
@@ -28,6 +28,7 @@ msbuild ./test/SdkTests/SdkTests.csproj /t:restore /p:TargetFramework=net45 /ver
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 
 Write-Host "ReBuilding SdkTests for Net4.5" -ForegroundColor Yellow
+dotnet restore ./test/SdkTests/SdkTests.csproj
 msbuild ./test/SdkTests/SdkTests.csproj /t:rebuild /verbosity:minimal
 if ($lastExitCode -ne 0) { exit $lastExitCode }
 }
